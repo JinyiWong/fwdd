2ed5bd3ea42510c6b2365baa0e6d0c7a0bf60029
Jinyi <jinyi@Wongs-MacBook-Air.local>
2025-10-29 13:59:54 +0800

    frontend ui

--- app.js
+++ app.js
@@ -17,6 +17,46 @@ app.get("/", function (req, res) {
 res.render("index", { title: "Hey", message: "Hello there!" });
 });
 
+app.get("/about", function (req, res) {
+res.render("about", { title: "About Us", message: "Learn more about our platform." });
+});
+
+app.get("/signin", function (req, res) {
+res.render("signin", { title: "Sign In", message: "Access your account." });
+});
+
+app.get("/register", function (req, res) {
+res.render("register");
+});
+
+app.get("/dashboard", function (req, res) {
+res.render("dashboard");
+});
+
+app.get("/leaderboard", function (req, res) {
+res.render("leaderboard");
+});
+
+app.get("/history", function (req, res) {
+res.render("history");
+});
+
+app.get("/results", function (req, res) {
+res.render("results");
+});
+
+app.get("/lobby", function (req, res) {
+res.render("lobby");
+});
+
+app.get("/room", function (req, res) {
+res.render("room");
+});
+
+app.get("/question", function (req, res) {
+res.render("question");
+});
+
 //6. Start the Server:
 const PORT = 3000;
 app.listen(PORT, () => {
